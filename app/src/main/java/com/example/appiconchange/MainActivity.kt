661f5362516bb19ddddfe2fe60685ba61ead9f11
apple <18201497620@163.com>
2021-03-16 12:47:45 +0800

    动态修改图标功能完成

--- app/src/main/java/com/example/appiconchange/MainActivity.kt
+++ app/src/main/java/com/example/appiconchange/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.appiconchange
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ComponentName
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+    private var mPackageManager: PackageManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnFesterval.setOnClickListener {
+        mPackageManager = applicationContext.packageManager
 
+        ivIconFesterval.setOnClickListener {
+            var festervalIcon = ComponentName(baseContext, "com.example.appiconchange.mainActivity")
+            enableComponent(festervalIcon)
+            disableComponent(componentName)
         }
 
-        btnNomal.setOnClickListener {
-
+        ivIconNomal.setOnClickListener {
+            var nomalIcon = ComponentName(baseContext, "com.example.appiconchange.MainActivity")
+            enableComponent(nomalIcon)
+            disableComponent(componentName)
         }
+    }
+
+    //启用组件
+    private fun enableComponent(componentName: ComponentName){
+        mPackageManager?.setComponentEnabledSetting(
+            componentName,
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP
+        )
+    }
+
+    //隐藏组件
+    private fun disableComponent(componentName: ComponentName){
+        mPackageManager?.setComponentEnabledSetting(
+            componentName,
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP
+        )
     }
 }
